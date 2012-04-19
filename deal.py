8e7af3606b04ac5a7628a40a6e5650e8e387cde0
James <ilsh1022@gmail.com>
2012-04-19 13:36:09 +0800

    first commit

---
+++ deal.py
@@ -0,0 +1,99 @@
+#coding:utf-8
+import os
+import sys
+import io
+import string
+import codecs
+import chardet
+from urllib2 import urlopen
+
+
+#find the url of next page
+def find_url(begin, end, content):
+       if type(content) is str:
+              content = content.decode('utf-8')
+       if not content:
+              return ''
+       start = content.find(begin)
+       if start == 0:
+              return None
+       test = content.find('current"') 
+       eee = content.find('href="',test)
+       test = content.find('http',eee)
+       eee = content.find('"',test)
+       return content[test:eee].strip()
+
+
+def txt_wrap_all(begin, end, url,num):
+#可以用来控制不全部扫描，而只扫描前多少页面
+#    if num > 5:
+#         return
+    num = num + 1
+    content = urlopen(url).read()
+    result = chardet.detect(content)
+    content = content.decode('gbk','ignore')
+    #content = content.decode(result['encoding'])
+    content = content.encode('utf-8')
+#    print content
+    if type(content) is str:
+           content = content.decode('utf-8')
+    if not content:
+           return ''
+#    result = []
+    from_pos = 0
+    if num == 1:
+          temp = open('paper.txt','w')
+    else:
+          temp = open('paper.txt','a')
+    temp.write('')
+    temp.close()
+    while True:
+        start = content.find(begin, from_pos)
+        if start >= 0:
+            start = start + len(begin)
+            endpos = content.find(end, start)
+            if endpos >= 0:
+#                result.append(html[start:endpos].strip())
+                temp = open('paper.txt','a')
+                temp.write(content[start:endpos].strip().encode('utf-8'))
+                temp.close()
+                #print html[start:endpos].strip().encode('utf-8') 
+#                result = []
+                from_pos = endpos+len(end)
+                continue
+        break
+    label = '下一页'
+    mark = '末页'
+    label = label.decode('utf-8')
+    mark = mark.decode('utf-8')
+    url = find_url(label,mark,content)
+    if url is None:
+        return
+    print url
+    txt_wrap_all(begin,end,url,num)
+    return 
+
+
+#get content from web begin
+#冒死记录
+#url = "http://www.tianya.cn/techforum/content/16/1/627945.shtml"
+#五大贼王
+url = "http://www.tianya.cn/techforum/content/16/386/623828.shtml"
+#get content from web end
+#write to file begin
+#temp = open("content",'w')
+#temp.write(content)
+#temp.close()
+#write to file close
+
+#content = open('content').read()
+#print content 
+
+begin = '作者：<a href="http://my.tianya.cn/368547" target="_blank">老夜</a>'
+end = '<div class="post-jb">'
+begin = begin.decode('utf-8')
+end = end.decode('utf-8')
+num = 10
+txt_wrap_all(begin,end,url,num)
+
+
